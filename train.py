24d46ceb1df5119959b7fc8da2b4ded07645b765
vincentdiete <217622395+vincentdiete@users.noreply.github.com>
2026-05-14 10:06:06 +0200

    Changes: (x,y) zu (Theta, r), 3 Monster types, more

---
+++ train.py
@@ -0,0 +1,15 @@
+import gymnasium
+from gymnasium import spaces
+import numpy as np
+import matplotlib.pyplot as plt
+import stable_baselines3
+from src.environment import Environment
+import pygame
+import time
+
+
+# Training
+env = Environment()
+model = stable_baselines3.SAC("MlpPolicy", env, verbose=1, tensorboard_log="./logs")
+model.learn(total_timesteps=100000)
+model.save("shooter_ppo")
